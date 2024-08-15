217b1e3cb0e35c073158dd1212255c35cf863b4d
Vijay Kumar <vijaykumar.m8564@gmail.com>
2024-08-15 18:06:55 +0530

    public-ip added

--- network_interface.tf
+++ network_interface.tf
@@ -1,3 +1,15 @@
+# pubic ip adding to nic
+
+resource "azurerm_public_ip" "example" {
+    name                = "example-pip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+
+
 resource "azurerm_network_interface" "example" {
     name = "vm-nic"
     location = azurerm_resource_group.example.location
@@ -6,7 +18,11 @@ resource "azurerm_network_interface" "example" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+
+    
 
    
   }
 }
+
