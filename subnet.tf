2d735b45895cd5a180ad5466b246ef49d237b47b
Vijay Kumar <vijaykumar.m8564@gmail.com>
2024-08-15 17:16:42 +0530

    subnet added

---
+++ subnet.tf
@@ -0,0 +1,8 @@
+resource "azurerm_subnet" "example" {
+    name = var.subnet_name
+    resource_group_name = azurerm_resource_group.example.name
+    virtual_network_name = azurerm_virtual_network.example.name
+    address_prefixes = var.subnet1_prefix
+
+    depends_on = [azurerm_virtual_network.example]
+}
--- variables.tf
+++ variables.tf
@@ -22,3 +22,15 @@ variable "address_space" {
     type        = list(string)
     default     = ["10.0.0.0/16"]
 }
+
+variable "subnet_name" {
+    description = "subnet_name"
+    type        = string
+    default     = "subnet-1"
+}
+
+variable "subnet1_prefix" {
+    description = "subnet-1 address prefix"
+    type        = list(string)
+    default     = ["10.0.1.0/24"]
+}
