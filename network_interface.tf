b9b99d70dfea9d279ccf63afad579ff09dd5f708
Vijay Kumar <vijaykumar.m8564@gmail.com>
2024-08-15 22:35:13 +0530

    nsg created and attached to nic with ssh rule

--- network_interface.tf
+++ network_interface.tf
@@ -8,21 +8,47 @@ resource "azurerm_public_ip" "example" {
   sku                 = "Standard"
 }
 
+# Create the Network Security Group 
+
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "allow_ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+}
+
+
 
 
 resource "azurerm_network_interface" "example" {
     name = "vm-nic"
     location = azurerm_resource_group.example.location
     resource_group_name = azurerm_resource_group.example.name
+
     ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.example.id
-
     
-
-   
+    
   }
+
 }
 
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
