c66fd1b6c07ad4cee935b866431c27aa458fb86a
Vijay Kumar <vijaykumar.m8564@gmail.com>
2024-08-16 16:44:52 +0530

    3 regions added

--- dynamic-resource-creation/variables.tf
+++ dynamic-resource-creation/variables.tf
@@ -1,23 +1,23 @@
 variable "rg_name" {
     description = "rg name"
     type        = list(string)
-    default     = ["rg1","rg2"]
+    default     = ["rg1","rg2","rg3"]
 }
 
 variable "rg_location" {
     description  = "rg location"
     type         = list(string)
-    default      = ["eastus","southindia"]
+    default      = ["eastus","southindia","westus"]
 }
 
 variable "vnet_name" {
     description = "vnet names"
     type        = list(string)
-    default     = ["vnet-1","vnet-2"]
+    default     = ["vnet-1","vnet-2","vnet-3"]
 }
 
 variable "address_space" {
     description = "address space"
     type        = list(string)
-    default     = ["10.1.0.0/16","10.2.0.0/16"]
+    default     = ["10.1.0.0/16","10.2.0.0/16","10.3.0.0/16"]
 }
